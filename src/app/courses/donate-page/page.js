e3174d921ffed661a1e25dde8027d39b2cf5b59f
dihasdinsara <chaminda.email@gmail.com>
2025-02-23 17:51:23 +0530

    updates

--- src/app/courses/donate-page/page.js
+++ src/app/courses/donate-page/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaWhatsapp, FaEnvelope } from 'react-icons/fa';
+import { FaWhatsapp, FaEnvelope } from "react-icons/fa";
 
 export default function page() {
   return (
@@ -11,20 +11,29 @@ export default function page() {
           </h1>
           <p className="text-gray-700 text-lg mb-8">
             Your generous donations help us to continue providing high-quality
-            educational content and resources for free. By supporting us, you are
-            enabling us to reach more students and make a greater impact in the
-            field of IoT and web development.
+            educational content and resources for free. By supporting us, you
+            are enabling us to reach more students and make a greater impact in
+            the field of IoT and web development.
           </p>
           <div className="flex justify-center space-x-4 mb-4">
-            <a href="https://wa.me/0719599991" target="_blank" rel="noopener noreferrer" className="inline-flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <a
+              href="https://wa.me/0719599991"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
               <FaWhatsapp className="mr-2" /> WhatsApp
             </a>
-            <a href="mailto:your-email@example.com" className="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <a
+              href="mailto:your-email@example.com"
+              className="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
               <FaEnvelope className="mr-2" /> Contact Us
             </a>
           </div>
           <p className="mt-4 text-sm text-gray-500">
-            Thank you for your support
+            We appreciate your kindness and generosity in supporting our
+            mission.
           </p>
         </div>
       </div>
